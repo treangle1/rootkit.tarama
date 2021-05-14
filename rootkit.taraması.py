c8869002a7fd7f39d16a5513010c295c94809b54
TREANGLE <73377812+fatih409@users.noreply.github.com>
2021-05-14 04:37:02 +0300

    Rootkit Tarama

---
+++ rootkit.taraması.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet Rootkit Taraması")
+
+print("Rootkit Tarama Aracına Hoşgeldiniz instagram = zumrudu_anka_team      ")
+print("Bu Araç Treangle Tarafından Oluşturulmuştur    ")
+os.system("chkrootkit")
